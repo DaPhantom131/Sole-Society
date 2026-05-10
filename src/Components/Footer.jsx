1cec0fc4b18a0cce1697ff40b4fb8c907d2e45d1
DaPhantom131 <danielokunor8@gmail.com>
2026-05-10 17:08:37 +0100

    having issues with react router dom

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react"
 const Footer = () => {
     return (
         <Footer className="Footer">
-            <p>2026 Sole Store. All rights reserved.</p>
+            <p>{Date.getFullYear()} Sole Store. All rights reserved.</p>
         </Footer>
     )
 }
