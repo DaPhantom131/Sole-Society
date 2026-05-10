ea016f9b4dd57bffcdf5b68c04e6149dd8cb620d
DaPhantom131 <danielokunor8@gmail.com>
2026-05-11 00:42:53 +0100

    final work completed

--- src/Components/Routes/Account.jsx
+++ src/Components/Routes/Account.jsx
@@ -1,21 +1,45 @@
 import React from "react";
+import "./Account.css"
 
 const Account = () => {
     return(
 
         <React.Fragment>
-            <form action="">
 
-              <label htmlFor="">UserName</label>
-              <input type="text" />
+            <div className="login-page">
+                <div className="login-wrapper">
+            <form action="" className="login-form">
 
-                <label htmlFor="">Password</label>
-                <input type="text" />
+                <div className="form-group">
+                    <label>UserName or Email</label>
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Enter your username"
+                        className="form-input"
+                        required
+                    />
+                </div>
 
-                <button>Login</button>
+                <div className="form-group">
+
+                <label>Password</label>
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Enter your password"
+                    className="form-input"
+                    required
+                />
+            </div>
+
+                <button type="submit" className="login-btn">Login</button>
 
                 <p>dont have an account? sign up</p>
+
             </form>
+                </div>
+            </div>
         </React.Fragment>
 
     )
